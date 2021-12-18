88ee9c217979a1bb0d548bb4d2b8764cc19953ed
Sergey Shimin <sergey.shimin84@gmail.com>
2021-12-18 19:22:06 +0700

    lesson-6.1

--- public/main.js
+++ public/main.js
@@ -1,18 +1,119 @@
 'use strict';
 
 // Вводится переменная с адресом корневого каталога (основная ссылка).
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/'
+const API_URL = 'http://127.0.0.1:3001/'
 
-Vue.component('search', {
+// 
+// @click - сокращенная запись v-on:click.
+Vue.component('good-card', {
   template: `
+    <div class="good-card" @click="onClick">
+      <h2>{{ data.title }}</h2>
+      <p>$ {{ data.price }}</p>
+    </div>
   `,
-  data() {
-    return {
-      
+  props: ['data'],
+  methods: {
+    onClick() {
+      fetch(`${API_URL}addToCart`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/JSON'
+        },
+        body: JSON.stringify(this.data)
+      })
+        .then(() => {
+          this.$emit('add', this.data)
+        })
     }
   }
 })
 
+Vue.component('goods-list', {
+  template: `
+    <div class="goods-list">
+      <good-card 
+        v-for="good of list" 
+        v-bind:key="good.id_product"
+        v-bind:data="good"
+        v-on:add="onAdd"
+      ></good-card>
+    </div>
+  `,
+  props: ['list'],
+  methods: {
+    onAdd(good) {
+      this.$emit('add', good)
+    }
+  }
+})
+
+Vue.component('search', {
+  template: `
+    <div class="search">
+      <input type="text" v-model="searchString" class="goods-search" />
+      <button class="search-button" type="button" v-on:click="onClick">Искать</button>
+    </div>
+  `,
+  data() {
+    return {
+      searchString: ''
+    }
+  },  
+  methods: {
+    onClick(){
+      this.$emit('search', this.searchString)
+    }
+  }
+})
+
+Vue.component('cart-item', {
+  template: `
+    <div class="good-card">
+      <h2>{{ data.title }}</h2>
+      <p>$ {{ data.price }}</p>
+      <button v-on:click="onClick">Удалить</button>
+    </div>
+  `,
+  props: ['data'],
+  methods: {
+    onClick() {
+      fetch(`${API_URL}removeFromCart`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/JSON'
+        },
+        body: JSON.stringify(this.data)
+      })
+        .then(() => {
+          this.$emit('delete', this.data)
+        })
+    }
+  }
+})
+
+Vue.component('cart', {
+  template: `
+    <div class="modal">
+      <cart-item 
+          v-for="good of list" 
+          v-bind:key="good.id_product"
+          v-bind:data="good"
+          v-on:delete="onDelete"
+        ></cart-item>
+        <button v-on:click="onClose">Закрыть</button>
+    </div>
+  `,
+  props: ['list'],
+  methods: {
+    onDelete(good) {
+      this.$emit('delete', good)
+    },
+    onClose(){
+      this.$emit('close')
+    }
+  }
+})
 
 // В объекте Vue находяться переменные (данные приложения).
 new Vue({
@@ -65,11 +166,22 @@ new Vue({
         })
     },
     // Метод фильтрации onSearch.
-    // Берется переменная this.searchLine. С помощью регулярного выражения проверяем названя товаров.
+    // Берется переменная this.searchString. С помощью регулярного выражения проверяем названя товаров.
     // Отфильтрованный массив помещаем в this.filteredGoods (найденый товар отрисован).
     onSearch() {
-      const reg = new RegExp(this.searchLine, 'i')
-      this.filteredGoods = this.goods.filter((good) => reg.test(good.product_name))
+      const regex = new RegExp(this.searchString, 'i')
+      this.filteredGoods = this.goods.filter((good) => regex.test(good.title))
+    },
+    // Метод onAdd добавляет товары.
+    onAdd(good) {
+      this.cart.push(good)
+    },
+    // Метод onDelete удаляет товары из корзины.
+    onDelete(good){
+      const idx = this.cart.findIndex((item) => item.id === good.id)
+      if(idx >= 0) {
+        this.cart = [...this.cart.slice(0, idx), ...this.cart.slice(idx + 1)]
+      }
     },
     // Метод onToggleCart переключает модальное окно, отрисовывая его.
     onToggleCart() {
