06bf34952245c01293bc1f032f6d676519194449
Sergey Shimin <sergey.shimin84@gmail.com>
2021-12-22 21:13:44 +0700

    lesson-7.1

--- archive/regular_expression.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const str = document.getElementsByTagName("p")['0'].textContent;
-const newstr = str.replace(/ \'/g, ' \"');
-const newstr2 = newstr.replace(/\' /g, '\" ');
-console.log(newstr2);
